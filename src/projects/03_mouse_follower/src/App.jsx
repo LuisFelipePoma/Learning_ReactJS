875707bde0af84033f18f003af1a47dbc632c6f4
Felipe <u202110902@upc.edu.pe>
2023-12-11 19:35:30 -0500

    add useEffect

--- src/projects/03_mouse_follower/src/App.jsx
+++ src/projects/03_mouse_follower/src/App.jsx
@@ -8,7 +8,7 @@ const FollowMouse = () => {
   useEffect(() => {
     console.log('effect ', { enabled })
 
-    const handleMove = (event) => {
+    const handleMove = event => {
       const { clientX, clientY } = event
       setPosition({ x: clientX, y: clientY })
     }
@@ -16,12 +16,12 @@ const FollowMouse = () => {
     if (enabled) {
       window.addEventListener('pointermove', handleMove)
     }
-
     // cleanup:
     // -> cuando el componente se desmonta
     // -> cuando cambian las dependencias, antes de ejecutar
     //    el efecto de nuevo
-    return () => { // cleanup method
+    return () => {
+      // cleanup method
       console.log('cleanup')
       window.removeEventListener('pointermove', handleMove)
     }
@@ -42,21 +42,26 @@ const FollowMouse = () => {
 
   return (
     <>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          display: `${enabled ? 'block' : 'none'}`,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
       />
-      <button onClick={() => setEnabled(!enabled)}>
+      <button
+        style={{ cursor: `${enabled ? 'none' : 'pointer'}` }}
+        onClick={() => setEnabled(!enabled)}
+      >
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
     </>
